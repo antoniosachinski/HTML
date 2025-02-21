f7b63a4907d96dfa9eff45c4f2dad8c847b979ff
Antonio Sachinski <as.sachinski@gmail.com>
2025-02-21 02:47:03 -0300

    Adição de html
    Códigos simples

---
+++ lado a lado/script.js
@@ -0,0 +1,16 @@
+function entrouEsquerda() {
+  indicadorLado.innerText = "Esquerda";
+  document.getElementById("indicadorLado").style.color = "green"
+  }
+  function saiuEsquerda() {
+    indicadorLado.innerText = "De qual lado está?";
+    document.getElementById("indicadorLado").style.color = "black"
+  }
+  function entrouDireita() {
+    indicadorLado.innerText = "Direita";
+    document.getElementById("indicadorLado").style.color = "purple"
+  }
+  function saiuDireita() {
+    indicadorLado.innerText = "De qual lado está?";
+    document.getElementById("indicadorLado").style.color = "black"
+  }
